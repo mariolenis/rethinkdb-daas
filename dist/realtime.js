a329cb5c44d1efb2656bd52ed9c8ed8a15dd9320
kerberus <kerberus@CTO-Kerberus>
2017-06-12 18:15:20 -0500

    Refactor to es6

--- dist/realtime.js
+++ dist/realtime.js
@@ -1,44 +1,50 @@
 "use strict";
-var db = require("./routes/db");
-var env_config_1 = require("./routes/env.config");
-var Realtime = (function () {
-    function Realtime(ioSocket) {
-        var _this = this;
+Object.defineProperty(exports, "__esModule", { value: true });
+const db = require("./routes/db");
+const env_config_1 = require("./routes/env.config");
+class Realtime {
+    constructor(ioSocket) {
         this.ioSocket = ioSocket;
         this.watcher = [];
-        this.ioSocket.on('connection', function (socket) {
+        this.ioSocket.on('connection', (socket) => {
             console.log('Client Connected ' + socket.id);
-            socket.on('join', function (conn, responseFn) {
+            socket.on('join', (conn, responseFn) => {
                 try {
-                    var connRequest_1 = JSON.parse(conn);
-                    console.log('[realtime.constructor] Connecting ' + socket.id + ' to room ' + connRequest_1.db + ' with API_KEY ' + connRequest_1.api_key);
+                    let connRequest = JSON.parse(conn);
+                    console.log('[realtime.constructor] Connecting ' + socket.id + ' to room ' + connRequest.db + ' with API_KEY ' + connRequest.api_key);
                     db.connectDB({ host: env_config_1.rethinkDBConfig.host, port: env_config_1.rethinkDBConfig.port, db: env_config_1.rethinkDBConfig.authDb })
-                        .flatMap(function (conn) { return db.auth(conn, connRequest_1.api_key); })
-                        .map(function (conn) { return conn.close(); })
-                        .flatMap(function () { return db.connectDB({ host: env_config_1.rethinkDBConfig.host, port: env_config_1.rethinkDBConfig.port, db: connRequest_1.db }); })
-                        .flatMap(function (conn) { return db.tableVerify(conn, connRequest_1.db, connRequest_1.table); })
-                        .map(function (conn) { return conn.open; })
-                        .subscribe(function () {
+                        .flatMap(conn => db.auth(conn, connRequest.api_key))
+                        .map(conn => conn.close())
+                        .flatMap(() => db.connectDB({ host: env_config_1.rethinkDBConfig.host, port: env_config_1.rethinkDBConfig.port, db: connRequest.db }))
+                        .flatMap(conn => db.tableVerify(conn, connRequest.db, connRequest.table))
+                        .map(conn => conn.open)
+                        .subscribe(() => {
                         socket.join(socket.id);
                         responseFn('ok');
-                    }, function () { return responseFn('err'); });
+                    }, () => responseFn('err'));
                 }
                 catch (e) {
                     console.error(e);
                     responseFn('err: ' + JSON.stringify(e));
                 }
             });
-            socket.on('listenChanges', function (message) { return _this.enrollChangeListener(message, socket.id); });
-            socket.on('disconnect', function () {
+            socket.on('listenChanges', (message) => this.enrollChangeListener(message, socket.id));
+            socket.on('disconnect', () => {
                 console.log("Client Disconnected " + socket.id);
+                let indexObserver = this.watcher.findIndex(w => w.id === socket.id);
+                if (indexObserver > -1) {
+                    console.log('Cleaning watcher ' + socket.id);
+                    this.watcher[indexObserver].subs.unsubscribe();
+                    this.watcher.splice(indexObserver);
+                }
             });
         });
     }
-    Realtime.prototype.enrollChangeListener = function (queryString, room) {
-        var query = JSON.parse(queryString);
-        var observer = this.watcher.filter(function (w) { return w.id === room; }).pop();
+    enrollChangeListener(queryString, room) {
+        let query = JSON.parse(queryString);
+        let observer = this.watcher.find(w => w.id === room);
         if (!observer) {
-            console.log('[realtime.enrollNameSpace] Enroll (' + room + ') for ' + query.table);
+            console.log('[realtime.enrollNameSpace] Enroll (' + room + ') for ' + JSON.stringify(query));
             this.watcher.push({
                 id: room,
                 subs: this.changesSubscription(query, room)
@@ -49,14 +55,15 @@ var Realtime = (function () {
             observer.subs.unsubscribe();
             observer.subs = this.changesSubscription(query, room);
         }
-    };
-    Realtime.prototype.changesSubscription = function (query, room) {
-        var _this = this;
+    }
+    changesSubscription(query, room) {
         return db.connectDB({ host: env_config_1.rethinkDBConfig.host, port: env_config_1.rethinkDBConfig.port, db: query.db })
-            .flatMap(function (conn) { return db.changes(conn, query); })
-            .subscribe(function (changes) { return _this.ioSocket.to(room).emit(query.table, JSON.stringify(changes)); });
-    };
-    return Realtime;
-}());
+            .flatMap(conn => db.changes(conn, query))
+            .subscribe(changes => {
+            console.log('Emitting changes to ' + room);
+            this.ioSocket.to(room).emit(query.table, JSON.stringify(changes));
+        });
+    }
+}
 exports.Realtime = Realtime;
 //# sourceMappingURL=realtime.js.map
